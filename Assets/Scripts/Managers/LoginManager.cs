349567ab6c78af2bc8700764e482fbdb913931cc
snesterenko <snesterenko@dtln.ru>
2023-04-16 22:44:09 +0300

    refactor: changed namespace for LoginManager

--- Assets/Scripts/Managers/LoginManager.cs
+++ Assets/Scripts/Managers/LoginManager.cs
@@ -2,14 +2,17 @@ using Controllers;
 using Mirror;
 using UnityEngine;
 
-public class LoginManager : NetworkManager
+namespace Managers
 {
-    [SerializeField] private GameController _gameController;
+    public class LoginManager : NetworkManager
+    {
+        [SerializeField] private GameController _gameController;
     
-    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
-    { 
-        base.OnServerAddPlayer(conn);
-        var player = conn.identity.gameObject.GetComponent<Player.Player>();
-        _gameController.AddNewPlayer(player);
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        { 
+            base.OnServerAddPlayer(conn);
+            var player = conn.identity.gameObject.GetComponent<Player.Player>();
+            _gameController.AddNewPlayer(player);
+        }
     }
 }
